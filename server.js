fbd1037a3446b1f1be8a997a4af553129a564694
Alper <alperuyanik4342@gmail.com>
2024-04-16 21:48:47 +0300

    navbar complated

---
+++ server.js
@@ -0,0 +1,37 @@
+
+const express = require('express') //importing the Express module
+const morgan = require('morgan')
+const app = express() //check express methods -> app.
+app.set('view engine', 'ejs')
+
+const port = 3000 //app listen port 3000
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
+
+app.use(express.static('public')) //static usage which is folder public 
+
+/*ara katman GET
+app.use((req, res, next) => {
+    console.log(req.method)
+    next();
+})*/ 
+
+app.use(morgan('tiny')) //middleware (ara katman) with morgan packet
+
+app.get('/' , (req, res) => {
+    res.render('index', {title: 'Anasayfa'})
+})
+
+app.get('/about' , (req, res) => {
+    res.render('about', {title: 'Hakkimizda'})
+})
+
+app.get('/about-us' , (req, res) => {
+    res.redirect('/about')
+})
+
+//error page must be after the get methods
+app.use((req, res) => {
+    res.status(404).render('404',{title: 'Sayfa Bulunamadı'})
+})
